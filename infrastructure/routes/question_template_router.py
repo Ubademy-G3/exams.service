733f75a6f1df023b2811d91decb73d096b3e7c12
GonzaloDiazMulligan <gonzalodiazmulligan@gmail.com>
2021-11-15 18:14:29 -0300

    fix ids requested from routes

--- infrastructure/routes/question_template_router.py
+++ infrastructure/routes/question_template_router.py
@@ -6,16 +6,19 @@ from application.services.auth import auth_service
 router = APIRouter()
 
 @router.post('/', response_model=QuestionTemplate, status_code = 201)
-async def create_question_template(question_template: QuestionTemplateSchema,
+async def create_question_template(exam_id: str,
+                                question_template: QuestionTemplateSchema,
                                 apikey: Optional[str] = Header(None)):
     return await QuestionTemplateController.create_question_template(question_template)
 
-@router.get('/{id}', response_model=QuestionTemplate, status_code = 200)
-async def get_question_template(id: str,
+@router.get('/{question_template_id}', response_model=QuestionTemplate, status_code = 200)
+async def get_question_template(exam_id: str,
+                                question_template_id: str,
                                 apikey: Optional[str] = Header(None)):
-    return await QuestionTemplateController.get_question_templates(id)
+    return await QuestionTemplateController.get_question_templates(question_template_id)
 
-@router.delete('/{id}')
-async def delete_question_template(id: str,
+@router.delete('/{question_template_id}')
+async def delete_question_template(exam_id: str,
+                                question_template_id: str,
                                 apikey: Optional[str] = Header(None)):
-    return await QuestionTemplateController.delete_question_templates(id)
+    return await QuestionTemplateController.delete_question_templates(question_template_id)
