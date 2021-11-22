885725252307eb84e7e46faaac1bba3163d4105c
GonzaloDiazMulligan <gonzalodiazmulligan@gmail.com>
2021-11-22 18:19:00 -0300

    fix typo

--- persistence/repositories/exam_solution_repository_postgres.py
+++ persistence/repositories/exam_solution_repository_postgres.py
@@ -13,36 +13,36 @@ class ExamSolutionRepositoryPostgres:
     def get_all_exam_solutions_by_exam_template_id(self, db, exam_template_id, graded, approval_state):
         query = db.query(ExamSolution).filter(ExamSolution.exam_template_id == exam_template_id)
         if graded is not None:
-            query = query.filter(ExamTemplate.graded == graded)
+            query = query.filter(ExamSolution.graded == graded)
         if approval_state is not None:
-            query = query.filter(ExamTemplate.approval_state == approval_state)
+            query = query.filter(ExamSolution.approval_state == approval_state)
         exam_solutions = query.all()
         return exam_solutions
 
     def get_all_exam_solutions_by_user_id(self, db, user_id, graded, approval_state):
         query = db.query(ExamSolution).filter(ExamSolution.user_id == user_id)
         if graded is not None:
-            query = query.filter(ExamTemplate.graded == graded)
+            query = query.filter(ExamSolution.graded == graded)
         if approval_state is not None:
-            query = query.filter(ExamTemplate.approval_state == approval_state)
+            query = query.filter(ExamSolution.approval_state == approval_state)
         exam_solutions = query.all()
         return exam_solutions
 
     def get_all_exam_solutions_by_corrector_id(self, db, corrector_id, graded, approval_state):
         query = db.query(ExamSolution).filter(ExamSolution.corrector_id == corrector_id)
         if graded is not None:
-            query = query.filter(ExamTemplate.graded == graded)
+            query = query.filter(ExamSolution.graded == graded)
         if approval_state is not None:
-            query = query.filter(ExamTemplate.approval_state == approval_state)
+            query = query.filter(ExamSolution.approval_state == approval_state)
         exam_solutions = query.all()
         return exam_solutions
 
     def get_all_exam_solutions_by_course_id(self, db, course_id, graded, approval_state):
         query = db.query(ExamSolution).filter(ExamSolution.course_id == course_id)
         if graded is not None:
-            query = query.filter(ExamTemplate.graded == graded)
+            query = query.filter(ExamSolution.graded == graded)
         if approval_state is not None:
-            query = query.filter(ExamTemplate.approval_state == approval_state)
+            query = query.filter(ExamSolution.approval_state == approval_state)
         exam_solutions = query.all()
         return exam_solutions
 
