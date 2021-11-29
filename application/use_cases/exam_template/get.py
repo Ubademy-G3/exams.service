3af5c450809d0ff3491368617e6debec5b02856c
GonzaloDiazMulligan <gonzalodiazmulligan@gmail.com>
2021-11-28 21:16:51 -0300

    trying to test a get_all method

--- application/use_cases/exam_template/get.py
+++ application/use_cases/exam_template/get.py
@@ -15,9 +15,11 @@ def get_exam_template(db, exam_template_id):
 def get_all_exam_templates_by_course_id(db, course_id, has_multiple_choice, has_written, has_media, state):
     exam_templates = etrp.get_all_exam_templates_by_course_id(db, course_id, has_multiple_choice,
                                                               has_written, has_media, state)
-
+    
+    print(type(exam_templates))
     exam_template_list = []
     for exam_template in exam_templates:
+        print(type(exam_template))
         exam_template_list.append(ExamTemplateSerializer.serialize(exam_template))
     return {
         "course_id": course_id,
--- tests/test_exam_templates.py
+++ tests/test_exam_templates.py
@@ -1,69 +1,133 @@
-"""import json
-import pytest
-import uuid
-from tests.conftest import test_app
-from persistence.repositories.course_repository_postgres import CourseRepositoryPostgres
-header = {"apikey": "@L4u71"}
-global_id = None
-test_request_payload = {
-    "name": "Python3",
-    "description": "asd",
-    "category": "coding",
-    "kind": "online",
-    "subscription_type": "free",
-    "location": "arg",
-    "info": {"hola": "as"}
+from fastapi.testclient import TestClient
+from main import app
+from unittest import TestCase, mock
+from persistence.repositories.exam_template_repository_postgres import ExamTemplateRepositoryPostgres
+from infrastructure.db.exam_template_schema import ExamTemplate, ExamStateEnum
+import json
+import os
+
+
+apikey = os.getenv("API_KEY")
+
+etrp = ExamTemplateRepositoryPostgres()
+
+
+client = TestClient(app)
+
+
+post_header = {"apikey": apikey}
+
+post_body = {
+    "course_id": "2f120281-12cd-413f-8e6a-2678b6b92406",
+    "creator_id": "f1e8ea4b-3909-4f66-80ca-aad491049bdf",
+    "name": "Test name for exam"
 }
-def test_create_course(test_app, monkeypatch):
-    async def mock_post(cls, payload):
-        return 1
-    monkeypatch.setattr(CourseRepositoryPostgres, "add_course", mock_post)
-    response = test_app.post("/courses/",data = json.dumps(test_request_payload),
-                            headers = header)
-    assert response.status_code == 201
-    response_json = response.json()
-    global global_id
-    global_id = response_json['id']
-    assert response_json['name'] == test_request_payload['name']
-    assert response_json['description'] == test_request_payload['description']
-    assert response_json['category'] == test_request_payload['category']
-    assert response_json['kind'] == test_request_payload['kind']
-    assert response_json['subscription_type'] == test_request_payload['subscription_type']
-    assert response_json['location'] == test_request_payload['location']
-    assert response_json['info'] == test_request_payload['info']
-def test_create_course_without_apikey(test_app, monkeypatch):
-    response = test_app.post("/courses/", data = json.dumps(test_request_payload))
-    assert response.status_code == 401
-    response_json = response.json()
-    assert response_json['error'] == "Error with API Key"
-def test_get_existing_course(test_app, monkeypatch):
-    course_id = global_id
-    test_response_payload = {
-        "id": course_id,
-        "name": "Python3",
-        "description": "asd",
-        "category": "coding",
-        "kind": "online",
-        "subscription_type": "free",
-        "location": "arg",
-        "info": {"hola": "as"}
-    }
-    async def mock_get(cls, id):
-        return test_response_payload
-    monkeypatch.setattr(CourseRepositoryPostgres, "get_course_by_id", mock_get)
-    response = test_app.get("/courses/"+str(course_id), headers = header)
-    assert response.status_code == 200
-    response_json = response.json()
-    assert response_json['id'] == course_id
-    assert response_json['name'] == test_request_payload['name']
-    assert response_json['description'] == test_request_payload['description']
-    assert response_json['category'] == test_request_payload['category']
-    assert response_json['kind'] == test_request_payload['kind']
-    assert response_json['subscription_type'] == test_request_payload['subscription_type']
-    assert response_json['location'] == test_request_payload['location']
-    assert response_json['info'] == test_request_payload['info']
-"""
 
 
-# def test_basic():
-#    assert 2 == 1 + 1
+class CreateExamTemplateMock(TestCase):
+
+    @mock.patch.object(ExamTemplateRepositoryPostgres, "add_exam_template")
+    def test_create_exam_template(self, mock_method):
+        mock_method.return_value = None
+
+        response = client.post(
+            "/exams/",
+            data=json.dumps(post_body),
+            headers=post_header
+        )
+        assert response.status_code == 201, response.text
+        data = response.json()
+        assert data["course_id"] == "2f120281-12cd-413f-8e6a-2678b6b92406"
+        assert data["creator_id"] == "f1e8ea4b-3909-4f66-80ca-aad491049bdf"
+        assert data["name"] == "Test name for exam"
+        assert data["state"] == "draft"
+
+
+get_header = {"apikey": apikey, "exam_id": "5122b737-f815-4e15-a56d-abbff2fee900"}
+
+get_body = None
+
+result_from_get = ExamTemplate(
+    id="5122b737-f815-4e15-a56d-abbff2fee900",
+    course_id="2f120281-12cd-413f-8e6a-2678b6b92406",
+    creator_id="f1e8ea4b-3909-4f66-80ca-aad491049bdf",
+    name="Test name for exam",
+    state=ExamStateEnum.draft,
+    max_score=10,
+    has_multiple_choice=False,
+    has_written=False,
+    has_media=False,
+    max_attempts=1,
+)
+
+
+class GetExamTemplateMock(TestCase):
+
+    @mock.patch.object(ExamTemplateRepositoryPostgres, "get_exam_template")
+    def test_create_exam_template(self, mock_method):
+        mock_method.return_value = result_from_get
+        exam_id = "5122b737-f815-4e15-a56d-abbff2fee900"
+        response = client.get(
+            f"/exams/{exam_id}",
+            headers=get_header
+        )
+        assert response.status_code == 200, response.text
+        data = response.json()
+        assert data["id"] == exam_id
+        assert data["course_id"] == "2f120281-12cd-413f-8e6a-2678b6b92406"
+        assert data["creator_id"] == "f1e8ea4b-3909-4f66-80ca-aad491049bdf"
+        assert data["name"] == "Test name for exam"
+        assert data["state"] == "draft"
+        assert data["max_score"] == 10
+        assert data["has_multiple_choice"] is False
+        assert data["has_written"] is False
+        assert data["has_media"] is False
+        assert data["max_attempts"] == 1
+
+
+get_all_by_course_id_header = {"apikey": apikey, "course_id": "2f120281-12cd-413f-8e6a-2678b6b92406"}
+
+get_all_by_course_id_body = None
+
+result_from_get_all_by_course_id = []
+
+result_from_get_all_by_course_id.append(
+    ExamTemplate(
+        id="5122b737-f815-4e15-a56d-abbff2fee900",
+        course_id="2f120281-12cd-413f-8e6a-2678b6b92406",
+        creator_id="f1e8ea4b-3909-4f66-80ca-aad491049bdf",
+        name="Test name for exam",
+        state=ExamStateEnum.draft,
+        max_score=10,
+        has_multiple_choice=False,
+        has_written=False,
+        has_media=False,
+        max_attempts=1,
+    )
+)
+
+
+class GetAllExamTemplatesByCourseIdMock(TestCase):
+
+    @mock.patch.object(ExamTemplateRepositoryPostgres, "get_all_exam_templates_by_course_id")
+    def test_create_exam_template(self, mock_method):
+        mock_method.return_value = result_from_get
+        course_id = "2f120281-12cd-413f-8e6a-2678b6b92406"
+        response = client.get(
+            f"/exams/course/{course_id}",
+            headers=get_header
+        )
+        assert response.status_code == 200, response.text
+        data = response.json()
+        assert data["course_id"] == course_id
+        assert data["amount"] == 1
+        assert data["exam_templates"][0]["id"] == "5122b737-f815-4e15-a56d-abbff2fee900"
+        assert data["exam_templates"][0]["course_id"] == course_id
+        assert data["exam_templates"][0]["creator_id"] == "f1e8ea4b-3909-4f66-80ca-aad491049bdf"
+        assert data["exam_templates"][0]["name"] == "Test name for exam"
+        assert data["exam_templates"][0]["state"] == "draft"
+        assert data["exam_templates"][0]["max_score"] == 10
+        assert data["exam_templates"][0]["has_multiple_choice"] is False
+        assert data["exam_templates"][0]["has_written"] is False
+        assert data["exam_templates"][0]["has_media"] is False
+        assert data["exam_templates"][0]["max_attempts"] == 1
